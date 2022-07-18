44c576c4dc72830fa5160300ec62265c85863571
liadk07 <91607695+liad07@users.noreply.github.com>
2022-07-18 21:58:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ const url=new URL(window.location.href)
 
 var str=url.searchParams.get("encode").trim()
 var newstr=""
-var color=""
     for (let i = 0; i < str.length; i++) {
         let ascii = str.charAt(i).charCodeAt(0);
         newstr+=String.fromCharCode(ascii-1)+""+String.fromCharCode(ascii+1)
     }
+//אין צורך לבדוק שהמספר זוגי הרי כל מספר אי זוגי או זוגי כפול 2 הוא זוגי
 document.write(newstr)
